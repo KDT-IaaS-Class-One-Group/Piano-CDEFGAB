5d9bf4c8884d0947e62a4f3745e82d2c14da41e4
최은철 <wndqhrgkdlel@gmail.com>
2023-10-12 16:16:43 +0900

    Element Design Fixes #3

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+for ( let i = 0; i < 7; i++){
+    ctx.fillRect(i*50, 50, 50, 300);
+}
+
+canvas.addEventListener('click',(e)=>{
+    console.log(e);
+})
