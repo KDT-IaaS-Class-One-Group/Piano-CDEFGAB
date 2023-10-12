408a8d06dfb61c78ce1cf4b3efe3d90f69c58cf6
최은철 <wndqhrgkdlel@gmail.com>
2023-10-12 16:27:38 +0900

    1개 상자 생성 후 클릭이벤트

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 const root = document.getElementById('root');
 const piano = document.getElementById('piano');
 const context = piano.getContext('2d');
+
+for ( let i = 0; i < 1; i++){
+    context.fillRect((i-1)*50, 0, 100, 300);
+}
+
+piano.addEventListener('click',(e)=>{
+    console.log(e.target);
+})
